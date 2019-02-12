5dc2360a73a4860baaf737af9b34f0090d3e7a71
Niwyn <theo.bremaud@gmail.com>
2019-02-12 09:01:23 +0100

    a

---
+++ precache-manifest.127dbb64e89a902452a3b20500852d40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "841ad50735e177c46f0f",
+    "url": "/js/about.fbf8f4df.js"
+  },
+  {
+    "revision": "d81e4cacb3c3ca3cb028",
+    "url": "/css/app.85d979d0.css"
+  },
+  {
+    "revision": "d81e4cacb3c3ca3cb028",
+    "url": "/js/app.597de540.js"
+  },
+  {
+    "revision": "aaeb40f1413aa06e2d38",
+    "url": "/js/chunk-vendors.d84a2034.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f8ace4814e0d97ed61b06738422c9090",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
